c8946f4d9856ff1a73e567a119298cea1ce75b98
Shaiful Aiman Malik <ibnumalik@tbd.my>
2019-04-28 13:18:12 +0800

    Add users table migration and seed

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => config('app.admin.name'),
+            'email' => config('app.admin.email'),
+            'password' => app('hash')->make(config('app.admin.password')),
+            'role' => 'admin'
+        ]);
+    }
+}
