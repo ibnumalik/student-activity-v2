e0bacf221388c18d4841db775dfe65d4327bd187
Shaiful Aiman Malik <ibnumalik@tbd.my>
2019-04-28 21:20:11 +0800

    Register user as student if role is not given

--- database/migrations/2019_04_28_035037_create_users_table.php
+++ database/migrations/2019_04_28_035037_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('role');
+            $table->string('role')->default('student');
             $table->string('token')->nullable();
             $table->timestamps();
         });
