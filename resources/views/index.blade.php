2082e6ad7fea9b310cdf921691078540012e3d03
Shaiful Aiman Malik <ibnumalik@tbd.my>
2019-04-30 16:56:11 +0800

    Load Material icons in html

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,10 +2,11 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>School CMS</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
