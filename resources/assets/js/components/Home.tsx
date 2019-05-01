7d7e2a63b1490f5cbb33638b3676aef5dfd1c10d
Shaiful Aiman Malik <ibnumalik@tbd.my>
2019-05-02 05:54:21 +0800

    Refactor home component code style

--- resources/assets/js/components/Home.tsx
+++ resources/assets/js/components/Home.tsx
@@ -1,45 +1,56 @@
 import * as React from 'react';
 import Button from '@material-ui/core/Button';
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import { createStyles, WithStyles, withStyles } from "@material-ui/core";
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { createStyles, WithStyles, withStyles, Theme } from '@material-ui/core';
 import * as PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const styles = createStyles({
-  root: {
-    flexGrow: 1
-  },
-  grow: {
-    flexGrow: 1
-  }
-})
+const styles = (theme: Theme) =>
+    createStyles({
+        root: {
+            flexGrow: 1
+        },
+        grow: {
+            flexGrow: 1
+        },
+        unlink: {
+            textDecoration: 'none',
+            color: 'rgba(0, 0, 0, 0.87)'
+        }
+    });
 
 export interface Props extends WithStyles<typeof styles> {}
 
 const HomeComponent = (props: Props) => {
-  const { classes } = props;
-  return (
-    <React.Fragment>
-      <AppBar position="static" color="default">
-        <Toolbar>
-          <Typography variant="h6" color="inherit" className={classes.grow}>
-            Student Portal
-          </Typography>
-          <Button color="inherit">
-            <Link to="login">
-            Login
-            </Link>
-          </Button>
-        </Toolbar>
-      </AppBar>
-    </React.Fragment>
-  );
+    const { classes } = props;
+    return (
+        <React.Fragment>
+            <AppBar position='static' color='default'>
+                <Toolbar>
+                    <Typography
+                        variant='h6'
+                        color='inherit'
+                        className={classes.grow}
+                    >
+                        <Link className={classes.unlink} to='/'>
+                            Student Portal
+                        </Link>
+                    </Typography>
+                    <Button color='inherit'>
+                        <Link className={classes.unlink} to='login'>
+                            Login
+                        </Link>
+                    </Button>
+                </Toolbar>
+            </AppBar>
+        </React.Fragment>
+    );
 };
 
 HomeComponent.prototype = {
-  classes: PropTypes.object.isRequired,
+    classes: PropTypes.object.isRequired
 } as any;
 
 export default withStyles(styles)(HomeComponent);
