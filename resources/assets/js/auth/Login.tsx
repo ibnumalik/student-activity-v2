d7898b12bfce85ce17aeafe9f649c7ce3aac00b0
Shaiful Aiman Malik <ibnumalik@tbd.my>
2019-05-02 05:27:18 +0800

    Refactor login component
    1. Code style formatting
    2. Refactor handle change email to handleChange

--- resources/assets/js/auth/Login.tsx
+++ resources/assets/js/auth/Login.tsx
@@ -10,133 +10,139 @@ import Typography from '@material-ui/core/Typography';
 import withStyles from '@material-ui/core/styles/withStyles';
 import { Theme, createStyles } from '@material-ui/core';
 import { WithStyles } from '@material-ui/core';
-import axios from "axios";
-import env from "../env";
+import axios from 'axios';
+import env from '../env';
 
-const styles = (theme: Theme) => createStyles({
-  main: {
-    width: 'auto',
-    display: 'block', // Fix IE 11 issue.
-    marginLeft: theme.spacing.unit * 3,
-    marginRight: theme.spacing.unit * 3,
-    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
-      width: 300,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    },
-  },
-  paper: {
-    marginTop: theme.spacing.unit * 8,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  loginHeader: {
-    padding: `${theme.spacing.unit * 2}px 0`,
-    width: '100%',
-    borderBottom: '1px solid rgba(0,0,0,0.12)'
-  },
-  loginBody: {
-    padding: `0 ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
-  },
-  avatar: {
-    margin: theme.spacing.unit,
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-  },
-  submit: {
-    marginTop: theme.spacing.unit * 3,
-  },
-});
+const styles = (theme: Theme) =>
+    createStyles({
+        main: {
+            width: 'auto',
+            display: 'block', // Fix IE 11 issue.
+            marginLeft: theme.spacing.unit * 3,
+            marginRight: theme.spacing.unit * 3,
+            [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+                width: 300,
+                marginLeft: 'auto',
+                marginRight: 'auto'
+            }
+        },
+        paper: {
+            marginTop: theme.spacing.unit * 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+        },
+        loginHeader: {
+            padding: `${theme.spacing.unit * 2}px 0`,
+            width: '100%',
+            borderBottom: '1px solid rgba(0,0,0,0.12)'
+        },
+        loginBody: {
+            padding: `0 ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`
+        },
+        avatar: {
+            margin: theme.spacing.unit,
+            backgroundColor: theme.palette.secondary.main
+        },
+        form: {
+            width: '100%' // Fix IE 11 issue.
+        },
+        submit: {
+            marginTop: theme.spacing.unit * 3
+        }
+    });
 
 interface LoginProps extends WithStyles<typeof styles> {}
 
 class Login extends React.Component<LoginProps> {
-  public state = {
-    email: '',
-    password: ''
-  }
-  private loginUrl = env.baseApi + '/login';
+    public state = {
+        formData: {
+            email: '',
+            password: ''
+        }
+    };
+    private loginUrl = env.baseApi + '/login';
 
-  private handleEmailAddressChange = (event: any) => {
-    this.setState({ email: event.target.value });
-  }
-
-  private handlePasswordChange = (event: any) => {
-    this.setState({ password: event.target.value });
-  }
-
-  private handleLogin = (event: any) => {
-    event.preventDefault();
-
-    const data = {
-      email: this.state.email,
-      password: this.state.password,
+    private handleChange = (event: any) => {
+        const { formData } = this.state;
+        formData[event.target.name] = event.target.value;
+        this.setState({ formData });
     };
 
-    axios.post(this.loginUrl, data)
-      .then(response => console.log(response));
+    private handleLogin = (event: any) => {
+        event.preventDefault();
 
-  }
+        const data = this.state.formData;
 
-  public render(): JSX.Element {
-    const classes = this.props.classes;
+        axios.post(this.loginUrl, data).then(response => console.log(response));
+    };
 
-    return (
-      <main className={classes.main}>
-        <Paper className={classes.paper}>
-          <div className={classes.loginHeader}>
-            <Typography component="h1" variant="h5" align="center">
-              Login
-            </Typography>
-          </div>
-          <div className={classes.loginBody}>
-            <form className={classes.form}>
-              <FormControl margin="normal" required fullWidth>
-                <InputLabel htmlFor="email">Email Address</InputLabel>
-                <Input
-                  onChange={this.handleEmailAddressChange}
-                  id="email"
-                  name="email"
-                  autoComplete="email"
-                  autoFocus
-                />
-              </FormControl>
-              <FormControl margin="normal" required fullWidth>
-                <InputLabel htmlFor="password">Password</InputLabel>
-                <Input
-                  onChange={this.handlePasswordChange}
-                  name="password"
-                  type="password"
-                  id="password"
-                  autoComplete="current-password" />
-              </FormControl>
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-                />
-              <Button
-                onClick={this.handleLogin}
-                type="submit"
-                fullWidth
-                variant="contained"
-                color="primary"
-                className={classes.submit}
-                >
-                Log in
-              </Button>
-            </form>
-          </div>
-        </Paper>
-      </main>
-    );
-  }
+    public render(): JSX.Element {
+        const classes = this.props.classes;
+
+        return (
+            <main className={classes.main}>
+                <Paper className={classes.paper}>
+                    <div className={classes.loginHeader}>
+                        <Typography component='h1' variant='h5' align='center'>
+                            Login
+                        </Typography>
+                    </div>
+                    <div className={classes.loginBody}>
+                        <form className={classes.form}>
+                            <FormControl margin='normal' required fullWidth>
+                                <InputLabel htmlFor='email'>
+                                    Email Address
+                                </InputLabel>
+                                <Input
+                                    onChange={this.handleChange}
+                                    id='email'
+                                    name='email'
+                                    autoComplete='email'
+                                    autoFocus
+                                />
+                            </FormControl>
+                            <FormControl margin='normal' required fullWidth>
+                                <InputLabel htmlFor='password'>
+                                    Password
+                                </InputLabel>
+                                <Input
+                                    onChange={this.handleChange}
+                                    name='password'
+                                    type='password'
+                                    id='password'
+                                    autoComplete='current-password'
+                                />
+                            </FormControl>
+                            <FormControlLabel
+                                control={
+                                    <Checkbox
+                                        value='remember'
+                                        color='primary'
+                                    />
+                                }
+                                label='Remember me'
+                            />
+                            <Button
+                                onClick={this.handleLogin}
+                                type='submit'
+                                fullWidth
+                                variant='contained'
+                                color='primary'
+                                className={classes.submit}
+                            >
+                                Log in
+                            </Button>
+                        </form>
+                    </div>
+                </Paper>
+            </main>
+        );
+    }
 }
 
 const login = () => {
-  console.log('test');
-}
+    console.log('test');
+};
 
 export default withStyles(styles)(Login);
