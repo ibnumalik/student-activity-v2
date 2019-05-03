134ae65d3ff1c46a77d7920ffc16246e17bde9b2
Shaiful Aiman Malik <ibnumalik@tbd.my>
2019-05-03 15:45:53 +0800

    Use validator in Login form

--- resources/assets/js/auth/LoginForm.tsx
+++ resources/assets/js/auth/LoginForm.tsx
@@ -9,6 +9,10 @@ import InputLabel from '@material-ui/core/InputLabel';
 import axios from 'axios';
 import env from '../env';
 import withStyles from '@material-ui/core/styles/withStyles';
+import FormValidator from '../forms/validator';
+
+type FormDataState = { email: string, password: string };
+type State = { formData: FormDataState, validation: LoginFormValidation };
 
 const styles = (theme: Theme) =>
     createStyles({
@@ -22,13 +26,37 @@ const styles = (theme: Theme) =>
 
 interface LoginFormProps extends WithStyles<typeof styles> {}
 
-class LoginForm extends React.Component<LoginFormProps> {
-    public state = {
+interface InputFieldValidation {
+    isInvalid: boolean;
+    message: String;
+}
+interface LoginFormValidation {
+    isValid: boolean;
+    email?: InputFieldValidation;
+    password?: InputFieldValidation;
+}
+
+class LoginForm extends React.Component<LoginFormProps, any> {
+    validator = new FormValidator([
+        {
+            field: 'email',
+            method: 'isEmpty',
+            validWhen: false,
+            message: 'Email is required'
+        },
+        {
+            field: 'password',
+            method: 'isEmpty',
+            validWhen: false,
+            message: 'Password is required'
+        }
+    ]);
+    state: State = {
         formData: {
             email: '',
             password: ''
         },
-        formError: ''
+        validation: this.validator.valid()
     };
     private loginUrl = env.baseApi + '/login';
 
@@ -41,33 +69,42 @@ class LoginForm extends React.Component<LoginFormProps> {
     private handleLogin = (event: any) => {
         event.preventDefault();
 
-        const data = this.state.formData;
+        const { formData } = this.state;
+        const validation: LoginFormValidation = this.validator.validate(
+            formData
+        );
 
-        axios
-            .post(this.loginUrl, data)
-            .then(response => {
-                console.log(response);
-            })
-            .catch(error => {
-                const {
-                    response: { status },
-                    response
-                } = error;
-                console.log(status, response);
-                if (status === 422) {
-                    this.setState({
-                        formError: 'Email/password is required'
-                    });
-                }
-            });
+        this.setState({ validation });
+
+        if (validation.isValid) {
+            axios
+                .post(this.loginUrl, formData)
+                .then(response => {
+                    console.log(response);
+                })
+                .catch(error => {
+                    const {
+                        response: { status },
+                        response
+                    } = error;
+                    console.log(status, response);
+                    if (status === 422) {
+                    }
+                });
+        }
     };
 
-    public render() {
+    render() {
         const classes = this.props.classes;
 
         return (
             <form className={classes.form}>
-                <FormControl margin='normal' required fullWidth>
+                <FormControl
+                    margin='normal'
+                    required
+                    fullWidth
+                    error={this.state.validation.email.isInvalid}
+                >
                     <InputLabel htmlFor='email'>Email Address</InputLabel>
                     <Input
                         onChange={this.handleChange}
